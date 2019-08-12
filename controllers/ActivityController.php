ab1a6699d783cb8da43bd9be12df851c7db73f90
igor193 <igor193@mail.ru>
2019-08-13 01:03:22 +0300

    correct files

---
+++ controllers/ActivityController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\controllers;
+
+use app\base\BaseController;
+use app\controllers\actions\ActivityCreateAction;
+use yii\web\Controller;
+
+
+class ActivityController extends BaseController {
+
+    public function actions () {
+        return [
+            'create'=>['class'=>ActivityCreateAction::class]
+        ];
+    }    
+}
---
+++ views/activity/create.php
@@ -0,0 +1,9 @@
+<?php
+
+?>
+
+<div class="row">
+    <div class="col-md-12">
+        <h3> Create activity</h3>
+    </div>
+</div>
